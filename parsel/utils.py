29753360dc26c9b6b902588d59cea43ee6b2913e
nanjiangwill <willjiang2018@gmail.com>
2024-11-15 13:01:54 -0800

    fix: Improve docstring formatting in parsel.utils

--- parsel/utils.py
+++ parsel/utils.py
@@ -2,12 +2,15 @@ import re
 from typing import Any, Iterable, Iterator, List, Match, Pattern, Union, cast
 from w3lib.html import replace_entities as w3lib_replace_entities
 
+
 def flatten(x: Iterable[Any]) -> List[Any]:
     """flatten(sequence) -> list
     Returns a single, flat list which contains all elements retrieved
     from the sequence and all recursively contained sub-sequences
     (iterables).
-    Examples:
+
+    Examples
+    --------
     >>> [1, 2, [3,4], (5,6)]
     [1, 2, [3, 4], (5, 6)]
     >>> flatten([[[1,2,3], (42,None)], [4,5], [6], 7, (8,9,10)])
@@ -16,6 +19,7 @@ def flatten(x: Iterable[Any]) -> List[Any]:
     ['foo', 'bar']
     >>> flatten(["foo", ["baz", 42], "bar"])
     ['foo', 'baz', 42, 'bar']
+
     """
     result = []
     for el in x:
@@ -25,18 +29,20 @@ def flatten(x: Iterable[Any]) -> List[Any]:
             result.append(el)
     return result
 
+
 def iflatten(x: Iterable[Any]) -> Iterator[Any]:
     """iflatten(sequence) -> Iterator
-    Similar to ``.flatten()``, but returns iterator instead"""
+    Similar to ``.flatten()``, but returns iterator instead
+    """
     for el in x:
         if _is_listlike(el):
             yield from iflatten(el)
         else:
             yield el
 
+
 def _is_listlike(x: Any) -> bool:
-    """
-    >>> _is_listlike("foo")
+    """>>> _is_listlike("foo")
     False
     >>> _is_listlike(5)
     False
@@ -61,7 +67,10 @@ def _is_listlike(x: Any) -> bool:
         and not hasattr(x, "items")
     )
 
-def extract_regex(regex: Union[str, Pattern[str]], text: str, replace_entities: bool=True) -> List[str]:
+
+def extract_regex(
+    regex: Union[str, Pattern[str]], text: str, replace_entities: bool = True
+) -> List[str]:
     """Extract a list of strings from the given text/encoding using the following policies:
     * if the regex contains a named group called "extract" that will be returned
     * if the regex contains multiple numbered groups, all those will be returned (flattened)
@@ -83,8 +92,9 @@ def extract_regex(regex: Union[str, Pattern[str]], text: str, replace_entities: 
 
     return [str(s) for s in extracted]
 
-def shorten(text: str, width: int, suffix: str='...') -> str:
+
+def shorten(text: str, width: int, suffix: str = "...") -> str:
     """Truncate the given text to fit in the given width."""
     if len(text) <= width:
         return text
-    return text[:width - len(suffix)] + suffix
+    return text[: width - len(suffix)] + suffix
