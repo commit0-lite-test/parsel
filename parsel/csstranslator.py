e155de4d58f807730f7f84e9c5965a672fcc9469
nanjiangwill <willjiang2018@gmail.com>
2024-11-15 12:58:08 -0800

    feat: Add support for CSS pseudo-elements in XPath translation

--- parsel/csstranslator.py
+++ parsel/csstranslator.py
@@ -1,12 +1,14 @@
 from functools import lru_cache
-from typing import TYPE_CHECKING, Any, Optional, Protocol
+from typing import TYPE_CHECKING, Optional, Protocol
 from cssselect import GenericTranslator as OriginalGenericTranslator
 from cssselect import HTMLTranslator as OriginalHTMLTranslator
 from cssselect.parser import Element, FunctionalPseudoElement, PseudoElement
 from cssselect.xpath import ExpressionError
 from cssselect.xpath import XPathExpr as OriginalXPathExpr
+
 if TYPE_CHECKING:
-    from typing_extensions import Self
+    pass
+
 
 class XPathExpr(OriginalXPathExpr):
     textnode: bool = False
@@ -15,20 +17,22 @@ class XPathExpr(OriginalXPathExpr):
     def __str__(self) -> str:
         path = super().__str__()
         if self.textnode:
-            if path == '*':
-                path = 'text()'
-            elif path.endswith('::*/*'):
-                path = path[:-3] + 'text()'
+            if path == "*":
+                path = "text()"
+            elif path.endswith("::*/*"):
+                path = path[:-3] + "text()"
             else:
-                path += '/text()'
+                path += "/text()"
         if self.attribute is not None:
-            if path.endswith('::*/*'):
+            if path.endswith("::*/*"):
                 path = path[:-2]
-            path += f'/@{self.attribute}'
+            path += f"/@{self.attribute}"
         return path
+
 
 class TranslatorProtocol(Protocol):
     pass
+
 
 class TranslatorMixin:
     """This mixin adds support to CSS pseudo elements via dynamic dispatch.
@@ -36,18 +40,20 @@ class TranslatorMixin:
     Currently supported pseudo-elements are ``::text`` and ``::attr(ATTR_NAME)``.
     """
 
-    def xpath_pseudo_element(self, xpath: OriginalXPathExpr, pseudo_element: PseudoElement) -> OriginalXPathExpr:
-        """
-        Dispatch method that transforms XPath to support pseudo-element
-        """
+    def xpath_pseudo_element(
+        self, xpath: OriginalXPathExpr, pseudo_element: PseudoElement
+    ) -> OriginalXPathExpr:
+        """Dispatch method that transforms XPath to support pseudo-element"""
         if isinstance(pseudo_element, FunctionalPseudoElement):
-            if pseudo_element.name == 'attr':
+            if pseudo_element.name == "attr":
                 return self.xpath_attr_functional_pseudo_element(xpath, pseudo_element)
-        elif isinstance(pseudo_element, Element) and pseudo_element.element == 'text':
+        elif isinstance(pseudo_element, Element) and pseudo_element.element == "text":
             return self.xpath_text_simple_pseudo_element(xpath)
         return xpath
 
-    def xpath_attr_functional_pseudo_element(self, xpath: OriginalXPathExpr, function: FunctionalPseudoElement) -> XPathExpr:
+    def xpath_attr_functional_pseudo_element(
+        self, xpath: OriginalXPathExpr, function: FunctionalPseudoElement
+    ) -> XPathExpr:
         """Support selecting attribute values using ::attr() pseudo-element"""
         if not function.arguments:
             raise ExpressionError("The ::attr() pseudo-element requires an argument.")
@@ -62,12 +68,17 @@ class TranslatorMixin:
         xpath.textnode = True
         return xpath
 
+
 class GenericTranslator(TranslatorMixin, OriginalGenericTranslator):
     pass
 
+
 class HTMLTranslator(TranslatorMixin, OriginalHTMLTranslator):
     pass
+
+
 _translator = HTMLTranslator()
+
 
 @lru_cache(maxsize=256)
 def css2xpath(query: str) -> str:
